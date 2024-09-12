a49b38bb4e0b76fd2add7813dfc3ca8ea7f4e32d
patoliyabhi7 <patoliyabhi7@gmail.com>
2024-09-12 17:02:31 +0530

    .

--- ReadEmails/app.js
+++ ReadEmails/app.js
@@ -4,13 +4,14 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 const blogRoute = require('./routes/blog');
-app.use(cors(
-    {
-        origin: ["https://google.com"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         origin: ["https://google.com"],
+//         methods: ["POST", "GET"],
+//         credentials: true
+//     }
+// ));
+app.use(cors());
 app.use(express.json());
 
 // Serve the index.html file from the root folder
